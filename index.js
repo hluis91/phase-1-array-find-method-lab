58967de56930cf7226f1a5a2d1b5f25b96a45dd2
Luis Hernandez <lshrnndz6@gmail.com>
2022-12-27 13:36:49 -0800

    commit tree 5f4b3b80d16e297f329fdcf4bbe735c626414d95

--- index.js
+++ index.js
@@ -1,1 +1,93 @@
 // code your solution here
+
+function superbowlWin(array){
+    const yearWin = array.find(win => win.result === 'W')
+    if(yearWin){
+        return yearWin.year
+    } else{
+        return undefined
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function superbowlWin(win) {
+//     const winFound = win.find(obj => obj.result === "W");
+//     if (winFound){
+//         return winFound.year;
+//     } else {
+//         return undefined;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
